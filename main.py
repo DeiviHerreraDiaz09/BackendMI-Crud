07a72c786a9dd35e6622a0a41762a4ce715e4311
DeiviHerreraDiaz09 <herreradiazdeivistiven2004@gmail.com>
2024-07-20 22:04:37 -0500

    FEAT: Cors habilited

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from api import userRouter
-
 
 app = FastAPI(
     title="Motoring Innovation",
@@ -13,5 +13,18 @@ app = FastAPI(
     },
 )
 
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+    "http://localhost:5173" 
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins, 
+    allow_credentials=True,
+    allow_methods=["*"], 
+    allow_headers=["*"], 
+)
 
 app.include_router(userRouter.router)
